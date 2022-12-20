496a2868d25d591d4d5b2e189d3e7d176071431c
刃心 <king-ty@foxmail.com>
2022-12-20 23:13:39 +0800

    Finish conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+        if let Some((x, y)) = s.split_once(',') {
+            if x.is_empty() {
+                return Person::default();
+            } else {
+                match y.parse() {
+                    Ok(age) => {
+                        return Person {
+                            name: x.to_string(),
+                            age,
+                        }
+                    }
+                    Err(_) => return Person::default(),
+                }
+            }
+        } else {
+            return Person::default();
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(Self::Err::Empty);
+        }
+        let terms: Vec<_> = s.split(',').collect();
+        if terms.len() != 2 {
+            return Err(Self::Err::BadLen);
+        }
+        if terms[0].is_empty() {
+            return Err(Self::Err::NoName);
+        }
+        match terms[1].parse() {
+            Ok(age) => Ok(Person {
+                name: terms[0].to_string(),
+                age,
+            }),
+            Err(err) => Err(Self::Err::ParseInt(err)),
+        }
     }
 }
 
